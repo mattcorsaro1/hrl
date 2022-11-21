1f98be443bde4cfa7a16b3404348d868a8a88d2e
Matt <matt.corsaro1@gmail.com>
2022-11-21 14:02:52 -0500

    Plotting path is not parameterized, so hard-code to save in scratch.

--- hrl/agent/dsc/utils.py
+++ hrl/agent/dsc/utils.py
@@ -116,7 +116,7 @@ def plot_two_class_classifier(option, episode, experiment_name, plot_examples=Tr
 
     name = option.name if episode is None else option.name + f"_{experiment_name}_{episode}"
     plt.title(f"{option.name} Initiation Set")
-    saving_path = os.path.join('results', experiment_name, 'initiation_set_plots', f'{name}_initiation_classifier_{seed}.png')
+    saving_path = os.path.join('~/scratch/results', experiment_name, 'initiation_set_plots', f'{name}_initiation_classifier_{seed}.png')
     plt.savefig(saving_path)
     plt.close()
 
@@ -166,7 +166,7 @@ def plot_initiation_distribution(option, mdp, episode, experiment_name, chunk_si
     plt.scatter(data[:, 0], data[:, 1], c=pvalues)
     plt.colorbar()
     plt.title("Density Estimator Fitted on Pessimistic Classifier")
-    saving_path = os.path.join('results', experiment_name, 'initiation_set_plots', f'{option.name}_initiation_distribution_{episode}.png')
+    saving_path = os.path.join('~/scratch/results', experiment_name, 'initiation_set_plots', f'{option.name}_initiation_distribution_{episode}.png')
     plt.savefig(saving_path)
     plt.close()
 
@@ -232,7 +232,7 @@ def make_chunked_goal_conditioned_value_function_plot(solver, goal, episode, see
     else:
         file_name = f"{solver.name}_value_function_seed_{seed}_episode_{episode}_option_{option_idx}"
     plt.title(f"VF Targeting {np.round(goal, 2)}")
-    saving_path = os.path.join('results', experiment_name, 'value_function_plots', f'{file_name}.png')
+    saving_path = os.path.join('~/scratch/results', experiment_name, 'value_function_plots', f'{file_name}.png')
 
     print("saving")
     plt.savefig(saving_path)
@@ -267,7 +267,7 @@ def plot_value_distribution(solver, state, goal, episode, option_name, seed, exp
     plt.xlabel("Expected Number of Steps to Goal")
     plt.title(f"Episode {episode} Goal {np.round(goal, 2)}")
     fname = f"{option_name}_value_dist{seed}_episode_{episode}.png"
-    plt.savefig(f"results/{experiment_name}/value_function_plots/{fname}")
+    plt.savefig(f"~/scratch/results/{experiment_name}/value_function_plots/{fname}")
     plt.close()
 
 
